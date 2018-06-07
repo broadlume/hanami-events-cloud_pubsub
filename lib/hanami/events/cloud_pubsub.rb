bc7d43cfb5f3334901d06bc462c4f6184a3a668c
Ian Ker-Seymer <i.kerseymer@gmail.com>
2018-06-06 20:59:55 -0400

    Allow logger to be configured on class

--- lib/hanami/events/cloud_pubsub.rb
+++ lib/hanami/events/cloud_pubsub.rb
@@ -5,11 +5,16 @@ require 'hanami/events/cloud_pubsub/version'
 require 'hanami/events/cloud_pubsub/runner'
 require 'hanami/events/cloud_pubsub/mixin'
 require 'google/cloud/pubsub'
+require 'dry-configurable'
 
 module Hanami
   module Events
     # CloudPubsub
     module CloudPubsub
+      extend Dry::Configurable
+
+      setting :logger, Logger.new(STDOUT), reader: true
+
       def self.setup
         Hanami::Events::Adapter.register(:cloud_pubsub) do
           require_relative 'adapter/cloud_pubsub'
--- spec/hanami/events/cloud_pubsub_spec.rb
+++ spec/hanami/events/cloud_pubsub_spec.rb
@@ -1,7 +1,33 @@
 # frozen_string_literal: true
 
+require 'dry/configurable/test_interface'
+
 RSpec.describe Hanami::Events::CloudPubsub do
+  before { described_class.enable_test_interface }
+  after { described_class.reset_config }
+
   it 'has a version number' do
     expect(Hanami::Events::CloudPubsub::VERSION).not_to be nil
+  end
+
+  describe '.logger' do
+    it 'allows for a logger to be configured' do
+      logger = :foo
+
+      described_class.configure do |config|
+        config.logger = logger
+      end
+
+      expect(described_class.logger).to eql(logger)
+    end
+
+    it 'defaults to a basic logger' do
+      expect(described_class.logger).to respond_to(
+        :info,
+        :warn,
+        :debug,
+        :error
+      )
+    end
   end
 end
