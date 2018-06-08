250a1185683ea63ddd5709ef81d3aa279565d06d
Ian Ker-Seymer <i.kerseymer@gmail.com>
2018-06-08 10:49:46 -0400

    Add configurable healthcheck server

--- examples/server.rb
+++ examples/server.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Hanami::Events::CloudPubsub.configure do |config|
+  config.subscriber.streams = 2
+  config.subscriber.threads.push = 2
+  config.subscriber.threads.callback = 2
+
   config.subscriptions_loader = -> do
     $events.subscribe('user.deleted', id: 'testing-2') do |payload|
       puts "Deleted2 user: #{payload}"
